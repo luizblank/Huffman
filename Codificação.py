959a862bc66c34e55389b9b9224ab321f18a2adc
luizblank <luizrosacardoso@gmail.com>
2022-03-24 16:03:47 -0300

    Update Codificação.py

--- Codificação.py
+++ Codificação.py
@@ -8,6 +8,7 @@ This is a temporary script file.
 lista = [4,4,4,4,4,4,6,8,8,8,7]
 repeticoes = []
 atribuicoes = []
+cod = []
 
 a = set(lista)
 a = list(a)
@@ -17,17 +18,26 @@ for i in a:
     repeticoes.append(b)
     
 c = len(repeticoes)
+mm = sorted(repeticoes, reverse = True)
 
 # for i in range(c):
 #     atribuicoes[a[i]] = repeticoes[i]
 
 indice = 0
+tamanho = len(a)
+
+num = "0"
+for i in range(tamanho):
+    cod.append(num)
+    num = num + "1"
     
 for i in range(c):
     atribuicoes.append(a[indice])
     atribuicoes.append(repeticoes[indice])
     indice = indice + 1
 
+lista2 = []
+
 for i in range(len(atribuicoes)):
     if (i%2) != 0:
-        print(bin(atribuicoes[i]))
+        lista2.append(i)
