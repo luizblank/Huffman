a2a83d2188a872ed6ddba2423f1bc49f94f8edb5
luizblank <luizrosacardoso@gmail.com>
2022-03-25 14:23:29 -0300

    Update Codificação.py

--- Codificação.py
+++ Codificação.py
@@ -9,6 +9,7 @@ lista = [4,4,4,4,4,4,6,8,8,8,7]
 repeticoes = []
 atribuicoes = []
 cod = []
+cod_certo=[]
 
 a = set(lista)
 a = list(a)
@@ -31,6 +32,10 @@ for i in range(tamanho):
     cod.append(num)
     num = num + "1"
     
+for i in cod:
+    i = i[::-1]
+    cod_certo.append(i)
+    
 for i in range(c):
     atribuicoes.append(a[indice])
     atribuicoes.append(repeticoes[indice])
@@ -41,3 +46,15 @@ lista2 = []
 for i in range(len(atribuicoes)):
     if (i%2) != 0:
         lista2.append(i)
+
+def apagar():
+    p = max(mm)
+    indice2 = mm.index(p)
+    mm.pop(indice2)
+    return p
+
+indice3 = 0
+for i in atribuicoes:
+    j = atribuicoes.index(apagar())-1
+    atribuicoes[j] = int(cod_certo[indice3])
+    indice3 = indice3 + 1
